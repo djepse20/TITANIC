1c3b548cc19122afc777cc5170acbbb3b954a6c5
unknown <djepse20@student.aau.dk>
2020-12-19 04:12:25 +0100

    xd

--- Makefile
+++
@@ -1,4 +0,0 @@
-GIT_COMMIT_HASH := "$(shell git describe --dirty --always --tags)"
-
-all:
-	gcc -DREVISION=\"$(GIT_COMMIT_HASH)\" -g -Wall -std=c99 -pedantic csv_parser/csv.c csv_parser/StrDup.c TimeSentinel.c userinteraction.c
--- csv_parser/StrDup.c
+++
@@ -1,8 +0,0 @@
-#include <string.h>
-#include "csv.h"
-char *StringDup(char *src) {
-    char *dst = malloc(strlen (src) + 1);  
-    if (dst == NULL) return NULL;         
-    strcpy(dst, src);                      
-    return dst;                            
-}
